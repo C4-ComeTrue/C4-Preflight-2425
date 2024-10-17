296d9f3d9d09b3899de0d696fc609225e4a121a0
hellozo0 <ahju0011@gmail.com>
2024-10-17 18:59:07 +0900

    feat : 메일 전송 요청하는 Controller 생성

---
+++ src/main/java/org/c4marathon/assignment/controller/MailSchedulerController.java
@@ -0,0 +1,27 @@
+package org.c4marathon.assignment.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.c4marathon.assignment.controller.request.MailSchedulerRequest;
+import org.c4marathon.assignment.service.MailSchedulerService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/backoffice")
+@RequiredArgsConstructor
+public class MailSchedulerController {
+
+    private final MailSchedulerService mailSchedulerService;
+
+    /**
+     * Step 1. 메일 전송 요청을 남기는 API 입니다.
+     */
+    @PostMapping("/mail")
+    public void postMailScheduler(@RequestBody MailSchedulerRequest request) {
+        mailSchedulerService.mailLogging(request);
+    }
+
+}
