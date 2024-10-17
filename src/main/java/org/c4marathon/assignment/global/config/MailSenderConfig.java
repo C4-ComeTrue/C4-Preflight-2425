81275966f1bd01ad47cc88eb8a905db813803565
ZZAMBAs <rksidksrksi@naver.com>
2024-10-17 21:12:30 +0900

    feat: 메일 발송 빈 추가

---
+++ src/main/java/org/c4marathon/assignment/global/config/MailSenderConfig.java
@@ -0,0 +1,42 @@
+package org.c4marathon.assignment.global.config;
+
+import java.util.Properties;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+public class MailSenderConfig {
+	@Value("${mail.username}")
+	private String username;
+	@Value("${mail.password}")
+	private String password;
+
+	@Bean
+	public JavaMailSender javaMailSender() {
+		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+
+		mailSender.setHost("smtp.naver.com");
+		mailSender.setPort(465);
+		mailSender.setUsername(username);
+		mailSender.setPassword(password);
+		mailSender.setJavaMailProperties(getMailProperties());
+
+		return mailSender;
+	}
+
+
+	private Properties getMailProperties() {
+		Properties properties = new Properties();
+		properties.setProperty("mail.transport.protocol", "smtp");
+		properties.setProperty("mail.smtp.auth", "true");
+		properties.setProperty("mail.smtp.starttls.enable", "true");
+		properties.setProperty("mail.debug", "true");
+		properties.setProperty("mail.smtp.ssl.trust","smtp.naver.com");
+		properties.setProperty("mail.smtp.ssl.enable","true");
+		return properties;
+	}
+}
