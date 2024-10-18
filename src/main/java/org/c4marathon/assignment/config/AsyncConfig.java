345e9f1aded1f321b2233952358828de571a0743
hellozo0 <ahju0011@gmail.com>
2024-10-19 00:53:35 +0900

    refactor : 불필요한 어노테이션 삭제

--- src/main/java/org/c4marathon/assignment/config/AsyncConfig.java
+++ src/main/java/org/c4marathon/assignment/config/AsyncConfig.java
@@ -2,14 +2,12 @@ package org.c4marathon.assignment.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.Executor;
 
 
 @Configuration
-@EnableAsync
 public class AsyncConfig {
 
     @Bean
