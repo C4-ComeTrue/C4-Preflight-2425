040cd34366fa78be7b8f26063708d878fd96b70c
ZZAMBAs <rksidksrksi@naver.com>
2024-10-18 15:59:07 +0900

    feat: 분 단위로 보내지 않은 메일들을 전송하는 메서드 추가

--- src/main/java/org/c4marathon/assignment/mail/application/MailService.java
+++ src/main/java/org/c4marathon/assignment/mail/application/MailService.java
@@ -1,22 +1,34 @@
 package org.c4marathon.assignment.mail.application;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.c4marathon.assignment.account.domain.AccountRepository;
 import org.c4marathon.assignment.mail.domain.Mail;
 import org.c4marathon.assignment.mail.domain.MailRepository;
 import org.c4marathon.assignment.mail.dto.CreateMailRequestDto;
 import org.c4marathon.assignment.mail.dto.CreateMailResponseDto;
+import org.c4marathon.assignment.mail.dto.MailInfoToSendDto;
 import org.c4marathon.assignment.user.domain.UserRepository;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Service
 public class MailService {
+	private static final int MAX_SEND_SIZE = 1000;
+
+	private final MailSender mailSender;
 	private final MailRepository mailRepository;
 	private final AccountRepository accountRepository;
 	private final UserRepository userRepository;
 
-	public MailService(MailRepository mailRepository, AccountRepository accountRepository,
+	public MailService(MailSender mailSender, MailRepository mailRepository, AccountRepository accountRepository,
 		UserRepository userRepository) {
+		this.mailSender = mailSender;
 		this.mailRepository = mailRepository;
 		this.accountRepository = accountRepository;
 		this.userRepository = userRepository;
@@ -32,4 +44,23 @@ public class MailService {
 
 		return CreateMailResponseDto.from(savedMail);
 	}
+
+	@Scheduled(cron = "0 * * * * *")
+	public void sendMail() {
+		List<MailInfoToSendDto> mailsToSend = mailRepository.findAllToSend(MAX_SEND_SIZE);
+		List<Long> sentMailIds = new ArrayList<>();
+
+		try {
+			mailsToSend.forEach(info -> {
+				mailSender.sendAccountCreateMsg(info);
+				sentMailIds.add(info.getMailId());
+			});
+		} catch (RuntimeException e) {
+			log.warn("메일 배치 발송 중 예외 발생", e);
+			log.warn("마지막으로 발송된 메일 id: {}", sentMailIds.get(sentMailIds.size() - 1));
+		}
+
+		mailRepository.updateSendTime(sentMailIds);
+	}
+
 }
