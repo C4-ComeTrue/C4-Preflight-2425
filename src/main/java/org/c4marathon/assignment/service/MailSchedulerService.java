4da10d57652067d65a259317d28274fdba28b084
hellozo0 <ahju0011@gmail.com>
2024-10-18 20:13:29 +0900

    feat : 스케줄링 구현 - 1분마다 MailLog Table에서 PENDING + FAIL 을 조회 - 메일을 보내는 handleMailSending() 호출 - 해당 스케줄링 함수는 호출될 일이 없으므로 protected로 설정

--- src/main/java/org/c4marathon/assignment/service/MailSchedulerService.java
+++ src/main/java/org/c4marathon/assignment/service/MailSchedulerService.java
@@ -1,6 +1,5 @@
 package org.c4marathon.assignment.service;
 
-
 import jakarta.mail.internet.MimeMessage;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
@@ -13,10 +12,11 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-
 import java.time.Instant;
-
+import java.util.Arrays;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -24,6 +24,7 @@ import java.time.Instant;
 public class MailSchedulerService {
     private final MailLogRepository mailLogRepository;
     private final JavaMailSender javaMailSender;
+    private static final String MAIL_SUBJECT = "계좌생성되었습니다";
 
     public void mailLogging(MailSchedulerRequest request) {
         mailLogRepository.save(new MailLog(request.userId(), request.email(), MailStatus.valueOf("PENDING"), request.content(), Instant.now(),Instant.now(), 0));
@@ -50,4 +51,12 @@ public class MailSchedulerService {
         mailLogRepository.save(mailLog);
     }
 
+    @Scheduled(cron = "0 * * * * *")
+    protected void mailSendScheduler() {
+        List<MailLog> mailLogs = mailLogRepository.findMailLogsByStatusIn(Arrays.asList(MailStatus.PENDING, MailStatus.FAIL));
+
+        for (MailLog mailLog : mailLogs) {
+            handleMailSending(new MailForm(mailLog.getId(), mailLog.getEmail(), MAIL_SUBJECT, mailLog.getContent()));
+        }
+    }
 }
