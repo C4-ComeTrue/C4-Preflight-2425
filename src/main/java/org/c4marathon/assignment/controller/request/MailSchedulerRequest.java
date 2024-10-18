542d5901d81e2454bef6e6940f67f2782f22ecd1
hellozo0 <ahju0011@gmail.com>
2024-10-18 20:05:19 +0900

    feat : MailRequest @Email 추가 - 유효하지 않은 이메일의 경우 Validation 추가

--- src/main/java/org/c4marathon/assignment/controller/request/MailSchedulerRequest.java
+++ src/main/java/org/c4marathon/assignment/controller/request/MailSchedulerRequest.java
@@ -1,5 +1,6 @@
 package org.c4marathon.assignment.controller.request;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
@@ -7,6 +8,7 @@ public record MailSchedulerRequest(
     int userId,
     @Size(max = 30)
     @NotNull
+    @Email
     String email,
     @Size(max = 500)
     String content
