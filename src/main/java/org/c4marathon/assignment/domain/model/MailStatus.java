6709c704f13e2e50f64d89a12cb0e6b330f38c37
hellozo0 <ahju0011@gmail.com>
2024-10-19 00:28:18 +0900

    refactor : 사용하지 않는 어노테이션 삭제

--- src/main/java/org/c4marathon/assignment/domain/model/MailStatus.java
+++ src/main/java/org/c4marathon/assignment/domain/model/MailStatus.java
@@ -1,10 +1,5 @@
 package org.c4marathon.assignment.domain.model;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
-@RequiredArgsConstructor
-@Getter
 public enum MailStatus {
     PENDING,
     SUCCESS,
