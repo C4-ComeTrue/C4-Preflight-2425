479d6851772bc52d5fae3de255a48df3e10059ab
hellozo0 <ahju0011@gmail.com>
2024-10-18 20:04:34 +0900

    feat : MailLogRepository 상태값을 기준으로 조회쿼리 작성 - 상태값 리스트를 통해 status에 해당하면 조회하는 쿼리문 작성

--- src/main/java/org/c4marathon/assignment/repository/MailLogRepository.java
+++ src/main/java/org/c4marathon/assignment/repository/MailLogRepository.java
@@ -1,9 +1,21 @@
 package org.c4marathon.assignment.repository;
 
 import org.c4marathon.assignment.domain.MailLog;
+import org.c4marathon.assignment.domain.model.MailStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface MailLogRepository extends JpaRepository<MailLog,Long> {
+public interface MailLogRepository extends JpaRepository<MailLog,Integer> {
+
+    @Query("""
+    SELECT m
+    FROM MailLog m
+    WHERE m.status IN :statuses
+    """)
+    List<MailLog> findMailLogsByStatusIn(@Param("statuses") List<MailStatus> statuses);
 }
