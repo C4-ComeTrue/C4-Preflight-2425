6e5e16e55ec6a91c2e58d960756fdbabfc63e6ae
ZZAMBAs <rksidksrksi@naver.com>
2024-10-19 21:32:07 +0900

    feat: 배치 처리를 메일이 안남을 때까지 계속 하도록 변경

--- src/main/java/org/c4marathon/assignment/mail/application/MailService.java
+++ src/main/java/org/c4marathon/assignment/mail/application/MailService.java
@@ -20,6 +20,7 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 public class MailService {
 	private static final int MAX_SEND_SIZE = 1000;
+	private static final int MAX_RETRY_COUNT = 3;
 
 	private final MailSender mailSender;
 	private final MailRepository mailRepository;
@@ -50,21 +51,39 @@ public class MailService {
 		log.debug("배치 작업 활성화");
 
 		List<MailInfoToSendDto> mailsToSend = mailRepository.findAllToSend(MAX_SEND_SIZE);
-		List<Long> sentMailIds = new ArrayList<>();
 
-		try {
-			mailsToSend.forEach(info -> {
-				mailSender.sendAccountCreateMsg(info);
-				sentMailIds.add(info.getMailId());
-			});
-		} catch (RuntimeException e) {
-			log.warn("메일 배치 발송 중 예외 발생", e);
-			log.warn("마지막으로 발송된 메일 id: {}", sentMailIds.get(sentMailIds.size() - 1));
+		while (!mailsToSend.isEmpty()) {
+			List<Long> sentMailIds = new ArrayList<>();
+
+			mailsToSend.forEach(info -> send(info, sentMailIds));
+
+			mailRepository.updateSendTime(sentMailIds);
+
+			mailsToSend = mailRepository.findAllToSend(MAX_SEND_SIZE);
 		}
 
-		mailRepository.updateSendTime(sentMailIds);
-
 		log.debug("배치 작업 정상 종료");
+	}
+
+	private void send(MailInfoToSendDto info, List<Long> sentMailIds) {
+		int retryCount = 0;
+		boolean isSent = false;
+
+		while (!isSent && retryCount < MAX_RETRY_COUNT) {
+			try {
+				mailSender.sendAccountCreateMsg(info);
+				sentMailIds.add(info.getMailId());
+				isSent = true;
+			} catch (RuntimeException e) {
+				log.warn("메일 배치 발송 중 예외 발생", e);
+				log.warn("해당 메일 id: {}", info.getMailId());
+				log.warn("현재까지 재시도 횟수: {}", retryCount++);
+			}
+		}
+
+		if (retryCount == MAX_RETRY_COUNT) {
+			log.error("id가 {}인 메일의 전송을 실패했습니다.", info.getMailId());
+		}
 	}
 
 }
