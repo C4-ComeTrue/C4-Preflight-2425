dd81ff5c67ccdc1c6960f412bdbd8daaca92a34a
hellozo0 <ahju0011@gmail.com>
2024-10-22 01:26:24 +0900

    refactor : Repository 코드 스타일 컨벤션 적용 - 불필요한 쿼리문 삭제 - JpaRepository, Repository 분리

--- src/main/java/org/c4marathon/assignment/repository/MailLogRepository.java
+++ src/main/java/org/c4marathon/assignment/repository/MailLogRepository.java
@@ -2,20 +2,23 @@ package org.c4marathon.assignment.repository;
 
 import org.c4marathon.assignment.domain.MailLog;
 import org.c4marathon.assignment.domain.model.MailStatus;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
 @Repository
-public interface MailLogRepository extends JpaRepository<MailLog,Integer> {
+public class MailLogRepository {
+    private final MailLogJpaRepository mailLogJpaRepository;
 
-    @Query("""
-    SELECT m
-    FROM MailLog m
-    WHERE m.status IN :statuses
-    """)
-    List<MailLog> findMailLogsByStatusIn(@Param("statuses") List<MailStatus> statuses);
+    public MailLogRepository(MailLogJpaRepository mailLogJpaRepository) {
+        this.mailLogJpaRepository = mailLogJpaRepository;
+    }
+
+    public List<MailLog> findMailLogsByStatusIn(List<MailStatus> statuses){
+        return mailLogJpaRepository.findMailLogsByStatusIn(statuses);
+    }
+
+    public void save(MailLog mailLog){
+        mailLogJpaRepository.save(mailLog);
+    }
+
 }
