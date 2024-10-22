822ee6dca48dda273ef143c5b8dc749653fca119
hellozo0 <ahju0011@gmail.com>
2024-10-23 02:22:58 +0900

    feat : 집계 Controller

---
+++ src/main/java/org/c4marathon/assignment/controller/StatisticsController.java
@@ -0,0 +1,40 @@
+package org.c4marathon.assignment.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.c4marathon.assignment.controller.request.DateRequest;
+import org.c4marathon.assignment.controller.request.RangeDateRequest;
+import org.c4marathon.assignment.controller.response.CumulativeAmountResponse;
+import org.c4marathon.assignment.service.CumulativeAmountService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/statics")
+@RequiredArgsConstructor
+public class StatisticsController {
+
+    private final CumulativeAmountService cumulativeAmountService;
+
+    /**
+     * 1. 특정 날짜의 통계를 강제로 집계 하는 API
+     *
+     */
+    @GetMapping("/cumulative/")
+    public ResponseEntity<CumulativeAmountResponse> getCumulativeAmountDate(@RequestParam @Valid DateRequest request) {
+        return ResponseEntity.ok(cumulativeAmountService.getCumulativeAmountDate(request));
+    }
+
+    /**
+     * 2. 주어진 날짜 사이의 통계값을 조회 하는 API
+     */
+    @GetMapping("/cumulative/range")
+    public ResponseEntity<List<CumulativeAmountResponse>> getCumulativeAmountRangeDate(@RequestParam @Valid RangeDateRequest request) {
+        return ResponseEntity.ok(cumulativeAmountService.getCumulativeAmountRangeDate(request));
+    }
+}
