6811f7c7c3185b1256731963f55f37891cdc235d
opixxx <opix0306@naver.com>
2024-10-24 17:41:21 +0900

    feat: Transaction 테이블 커서 페이징 기반 쿼리 구현
    - findTransaction : 첫 조회시 사용되는 쿼리
    - findTransactionWithEndDateAndLastDate : transaction_date, transaction_id 기반으로 커서 페이징 쿼리
       - endDate : 강제로 해당 날짜까지 통계 집계
       - lastDate : 그 전 쿼리에 마지막으로 조회한 날짜
       - lastId : Transaction 데이터는 같은 날짜에 데이터도 있기 떄문에 날짜가 같을 시 id 값을 기준으로 가져옴

---
+++ src/main/java/org/c4marathon/assignment/transaction/domain/Transaction.java
@@ -0,0 +1,65 @@
+package org.c4marathon.assignment.transaction.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "transaction")
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "transaction_id", nullable = false)
+    private Integer id;
+
+    @Size(max = 20)
+    @NotNull
+    @Column(name = "sender_account", nullable = false, length = 20)
+    private String senderAccount;
+
+    @Size(max = 20)
+    @NotNull
+    @Column(name = "receiver_account", nullable = false, length = 20)
+    private String receiverAccount;
+
+    @Size(max = 11)
+    @NotNull
+    @Column(name = "sender_swift_code", nullable = false, length = 11)
+    private String senderSwiftCode;
+
+    @Size(max = 11)
+    @NotNull
+    @Column(name = "receiver_swift_code", nullable = false, length = 11)
+    private String receiverSwiftCode;
+
+    @Size(max = 30)
+    @NotNull
+    @Column(name = "sender_name", nullable = false, length = 30)
+    private String senderName;
+
+    @Size(max = 30)
+    @NotNull
+    @Column(name = "receiver_name", nullable = false, length = 30)
+    private String receiverName;
+
+    @NotNull
+    @Column(name = "amount", nullable = false)
+    private Long amount;
+
+    @Size(max = 200)
+    @Column(name = "memo", length = 200)
+    private String memo;
+
+    @NotNull
+    @ColumnDefault("CURRENT_TIMESTAMP")
+    @Column(name = "transaction_date", nullable = false)
+    private Instant transactionDate;
+
+}
