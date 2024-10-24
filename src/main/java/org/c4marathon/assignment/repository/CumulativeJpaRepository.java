c9027d600f8019b1b12daf624d4790729427a304
hellozo0 <ahju0011@gmail.com>
2024-10-24 22:07:10 +0900

    feat : 누적 금액 Repository 날짜 조회
    - 날짜 별로 테이블에 있는지 조회하는 쿼리

--- src/main/java/org/c4marathon/assignment/repository/CumulativeJpaRepository.java
+++ src/main/java/org/c4marathon/assignment/repository/CumulativeJpaRepository.java
@@ -2,8 +2,17 @@ package org.c4marathon.assignment.repository;
 
 import org.c4marathon.assignment.domain.CumulativeAmount;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.Optional;
 
 @Repository
 public interface CumulativeJpaRepository extends JpaRepository<CumulativeAmount, Integer> {
+
+    @Query("SELECT c FROM CumulativeAmount c WHERE c.date = :date")
+    CumulativeAmount findByDate(@Param("date") LocalDate date);
 }
--- src/main/java/org/c4marathon/assignment/repository/CumulativeRepository.java
+++ src/main/java/org/c4marathon/assignment/repository/CumulativeRepository.java
@@ -1,10 +1,23 @@
 package org.c4marathon.assignment.repository;
 
 import lombok.RequiredArgsConstructor;
+import org.c4marathon.assignment.domain.CumulativeAmount;
 import org.springframework.stereotype.Repository;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
 public class CumulativeRepository {
     private final CumulativeJpaRepository cumulativeJpaRepository;
+
+    public CumulativeAmount findByDate(LocalDate date){
+        return cumulativeJpaRepository.findByDate(date);
+    }
+
+    public void save(CumulativeAmount cumulativeAmount){
+        cumulativeJpaRepository.save(cumulativeAmount);
+    }
 }
