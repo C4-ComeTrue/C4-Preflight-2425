ab8ca2cd7224e9a044ae73788b7f32bba9ed8a51
hellozo0 <ahju0011@gmail.com>
2024-10-24 21:57:51 +0900

    feat : Request 미사용으로 삭제

--- src/main/java/org/c4marathon/assignment/controller/StatisticsController.java
+++ src/main/java/org/c4marathon/assignment/controller/StatisticsController.java
@@ -1,17 +1,19 @@
 package org.c4marathon.assignment.controller;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
-import org.c4marathon.assignment.controller.request.DateRequest;
 import org.c4marathon.assignment.controller.request.RangeDateRequest;
 import org.c4marathon.assignment.controller.response.CumulativeAmountResponse;
 import org.c4marathon.assignment.service.CumulativeAmountService;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -25,9 +27,9 @@ public class StatisticsController {
      * 1. 특정 날짜의 통계를 강제로 집계 하는 API
      *
      */
-    @GetMapping("/cumulative/")
-    public ResponseEntity<CumulativeAmountResponse> getCumulativeAmountDate(@RequestParam @Valid DateRequest request) {
-        return ResponseEntity.ok(cumulativeAmountService.getCumulativeAmountDate(request));
+    @GetMapping("/cumulative")
+    public ResponseEntity<CumulativeAmountResponse> getCumulativeAmountDate( @RequestParam @NotBlank @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate date) {
+        return ResponseEntity.ok(cumulativeAmountService.getCumulativeAmountDate(date));
     }
 
     /**
