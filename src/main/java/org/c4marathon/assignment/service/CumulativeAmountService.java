af1ce760914cfa2b1a3da292c6cbc2c5f36de545
hellozo0 <ahju0011@gmail.com>
2024-10-25 16:06:26 +0900

    refactor : 불필요한 코드 삭제
    - 불필요한 조회 transaction 삭제

--- src/main/java/org/c4marathon/assignment/service/CumulativeAmountService.java
+++ src/main/java/org/c4marathon/assignment/service/CumulativeAmountService.java
@@ -28,8 +28,9 @@ public class CumulativeAmountService {
     private final CumulativeRepository cumulativeRepository;
 
     /**
-     * Transaction 거래 내역 List로 페이징 검색하는 로직
-     */
+     * API : statics/cumulative
+     * transaction의 ~ date(endDate) 까지의 일별/누적금액 계산하는 로직
+     * */
     public CumulativeAmountResponse getCumulativeAmountDate(LocalDate date){
 
         // 1. 해당 날짜의 통계 테이블이 있으면 return
@@ -61,72 +62,9 @@ public class CumulativeAmountService {
     }
 
     /**
-     * 해당 날짜까지의 누적 합 계산 로직
-     */
-    private void cumulativeAmountByDate(LocalDate date){
-        //1. 가장 이른 날짜 가져와서 반복문 돌릴 예정
-        Instant earliestTransactionDate = transactionRepository.findEarliestTransactionDate();
-        LocalDate startDate = earliestTransactionDate.atZone(ZoneOffset.UTC).toLocalDate();
-
-        long previousCumulativeTotal = 0L;
-        // 2. 첫날의 데이터를 처리
-        CumulativeAmount cumulativeAmount = cumulativeRepository.findByDate(startDate);
-        cumulativeAmount.updateCumulativeAmount(cumulativeAmount.getDailyAmount());
-        previousCumulativeTotal = cumulativeAmount.getCumulativeAmount();
-        cumulativeRepository.save(cumulativeAmount);
-
-        // 3. startDate의 다음 날부터 주어진 date까지의 데이터를 처리
-        //여기서부터 startDate부터 ~ date(주어진 날짜까지의 Date) 까지 일단 startDate는 첫날이라서 그날의 dailyAmount가 저장되고
-        // 그 이후 부터는 전날의 CumulativeAmount + 다음날의 dailtAMount 한 누적 금액이 저장이 되는 구조
-        for (LocalDate currentDate = startDate.plusDays(1); !currentDate.isAfter(date); currentDate = currentDate.plusDays(1)) {
-
-            // 현재 날짜의 누적합 계산 및 저장
-            CumulativeAmount currentDayDailyAmount = cumulativeRepository.findByDate(currentDate);
-            currentDayDailyAmount.updateCumulativeAmount(previousCumulativeTotal + currentDayDailyAmount.getDailyAmount());
-            previousCumulativeTotal = currentDayDailyAmount.getCumulativeAmount();
-            // 데이터 저장
-            cumulativeRepository.save(currentDayDailyAmount);
-        }
-
-    }
-
-    /**
-     * [1000개 단위로 묶인 데이터로 작업] 당일 Amount만 계산하는  로직
+     * API : statics/cumulative/range
+     * start ~ endDate 사이의 일별/누적 금액 다시 계산 하는 로직
      * */
-    private void dailyAmountByDate(List<Transaction> transactionList) {
-
-        //1. map에 날짜별로 2023-01-01 형태로, amount를 누적 합한다.
-        Map<LocalDate, Long> map = transactionList.stream()
-                .collect(Collectors.groupingBy(
-                        transaction -> transaction.getTransactionDate()
-                                .atZone(ZoneOffset.UTC)
-                                .toLocalDate(),
-                        Collectors.summingLong(Transaction::getAmount)
-                ));
-
-        //2. 해당 묶음을 updateCumulativeAmountByDate()함수에 전달한다.
-        map.forEach(this::updateCumulativeAmountByDate);
-    }
-
-    /**
-     * [1000개 단위로 묶인 데이터로 작업] 데이터들을 update - save 하는 함수
-     * */
-    private void updateCumulativeAmountByDate(LocalDate date, long dailyAmount){
-
-        //1. 먼저 이미 Repository에 먼저 저장되어 있는 1000개의 묶음 결과가 저장되어 있을 수 있으니 조회한다.
-        CumulativeAmount previousCumulativeAmount = cumulativeRepository.findByDate(date.minusDays(1));
-        CumulativeAmount cumulativeAmount = cumulativeRepository.findByDate(date);
-
-        //2. null이라면 첫번째 transaction이기 때문에 새로운 객체를 생성해서 저장.
-        if (cumulativeAmount == null) {
-            cumulativeAmount = new CumulativeAmount(date, dailyAmount, 0L);
-        } else {
-            cumulativeAmount.updateDailyAmount(cumulativeAmount.getDailyAmount() + dailyAmount);
-        }
-
-        cumulativeRepository.save(cumulativeAmount);
-    }
-
     public List<CumulativeAmountResponse> getCumulativeAmountRangeDate(LocalDate startDate, LocalDate endDate){
         List<CumulativeAmountResponse> cumulativeAmountResponses = new ArrayList<>();
 
@@ -165,6 +103,75 @@ public class CumulativeAmountService {
         return cumulativeAmountResponses;
     }
 
+    /**
+     * API : statics/cumulative
+     * > getCumulativeAmountDate > cumulativeAmountByDate
+     * 해당 날짜까지의 누적 합 계산 로직
+     */
+    private void cumulativeAmountByDate(LocalDate date){
+        //1. 가장 이른 날짜 가져와서 반복문 돌릴 예정
+        Instant earliestTransactionDate = transactionRepository.findEarliestTransactionDate();
+        LocalDate startDate = earliestTransactionDate.atZone(ZoneOffset.UTC).toLocalDate();
 
+        long previousCumulativeTotal = 0L;
+        // 2. 첫날의 데이터를 처리
+        CumulativeAmount cumulativeAmount = cumulativeRepository.findByDate(startDate);
+        cumulativeAmount.updateCumulativeAmount(cumulativeAmount.getDailyAmount());
+        previousCumulativeTotal = cumulativeAmount.getCumulativeAmount();
+        cumulativeRepository.save(cumulativeAmount);
 
+        // 3. startDate의 다음 날부터 주어진 date까지의 데이터를 처리
+        //여기서부터 startDate부터 ~ date(주어진 날짜까지의 Date) 까지 일단 startDate는 첫날이라서 그날의 dailyAmount가 저장되고
+        // 그 이후 부터는 전날의 CumulativeAmount + 다음날의 dailtAMount 한 누적 금액이 저장이 되는 구조
+        for (LocalDate currentDate = startDate.plusDays(1); !currentDate.isAfter(date); currentDate = currentDate.plusDays(1)) {
+
+            // 현재 날짜의 누적합 계산 및 저장
+            CumulativeAmount currentDayDailyAmount = cumulativeRepository.findByDate(currentDate);
+            currentDayDailyAmount.updateCumulativeAmount(previousCumulativeTotal + currentDayDailyAmount.getDailyAmount());
+            previousCumulativeTotal = currentDayDailyAmount.getCumulativeAmount();
+            // 데이터 저장
+            cumulativeRepository.save(currentDayDailyAmount);
+        }
+
+    }
+
+    /**
+     * API : statics/cumulative
+     * > getCumulativeAmountDate > dailyAmountByDate
+     * [1000개 단위로 묶인 데이터로 작업] 당일 Amount만 계산하는  로직
+     * */
+    private void dailyAmountByDate(List<Transaction> transactionList) {
+
+        //1. map에 날짜별로 2023-01-01 형태로, amount를 누적 합한다.
+        Map<LocalDate, Long> map = transactionList.stream()
+                .collect(Collectors.groupingBy(
+                        transaction -> transaction.getTransactionDate()
+                                .atZone(ZoneOffset.UTC)
+                                .toLocalDate(),
+                        Collectors.summingLong(Transaction::getAmount)
+                ));
+
+        //2. 해당 묶음을 updateCumulativeAmountByDate()함수에 전달한다.
+        map.forEach(this::updateCumulativeAmountByDate);
+    }
+
+    /**
+     * API : statics/cumulative
+     * > getCumulativeAmountDate > dailyAmountByDate > updateCumulativeAmountByDate
+     * [1000개 단위로 묶인 데이터로 작업] 데이터들을 update - save 하는 함수
+     * */
+    private void updateCumulativeAmountByDate(LocalDate date, long dailyAmount){
+
+        //1. 먼저 이미 Repository에 먼저 저장되어 있는 1000개의 묶음 결과가 저장되어 있을 수 있으니 조회한다.
+        CumulativeAmount cumulativeAmount = cumulativeRepository.findByDate(date);
+
+        //2. null이라면 첫번째 transaction이기 때문에 새로운 객체를 생성해서 저장.
+        if (cumulativeAmount == null) {
+            cumulativeAmount = new CumulativeAmount(date, dailyAmount, 0L);
+        } else {
+            cumulativeAmount.updateDailyAmount(cumulativeAmount.getDailyAmount() + dailyAmount);
+        }
+
+        cumulativeRepository.save(cumulativeAmount);
+    }
 }
