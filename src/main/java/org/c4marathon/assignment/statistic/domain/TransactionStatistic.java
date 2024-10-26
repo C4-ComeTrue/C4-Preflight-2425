98b1198649b5d37b68deabe9cef508ebe6e578c6
ZZAMBAs <rksidksrksi@naver.com>
2024-10-26 10:57:49 +0900

    feat: 빌더 추가

--- src/main/java/org/c4marathon/assignment/statistic/domain/TransactionStatistic.java
+++ src/main/java/org/c4marathon/assignment/statistic/domain/TransactionStatistic.java
@@ -8,11 +8,15 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
 @Table(name = "transaction_statistic_zzamba")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class TransactionStatistic {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,4 +30,11 @@ public class TransactionStatistic {
 
 	@Column(nullable = false)
 	private long cumulativeAmount;
+
+	@Builder
+	public TransactionStatistic(Instant statisticDate, long dailyTotalAmount, long cumulativeAmount) {
+		this.statisticDate = statisticDate;
+		this.dailyTotalAmount = dailyTotalAmount;
+		this.cumulativeAmount = cumulativeAmount;
+	}
 }
