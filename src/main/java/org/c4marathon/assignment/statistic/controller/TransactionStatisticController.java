a85122f158b3015fa532361f4089240e01b9b64c
ZZAMBAs <rksidksrksi@naver.com>
2024-10-26 11:59:29 +0900

    feat: 강제 통계 API 추가

--- src/main/java/org/c4marathon/assignment/statistic/controller/TransactionStatisticController.java
+++ src/main/java/org/c4marathon/assignment/statistic/controller/TransactionStatisticController.java
@@ -1,13 +1,26 @@
 package org.c4marathon.assignment.statistic.controller;
 
+import java.time.Instant;
+
 import org.c4marathon.assignment.statistic.application.TransactionStatisticService;
+import org.c4marathon.assignment.statistic.dto.TransactionStatisticResult;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/statistic/transaction")
 public class TransactionStatisticController {
 	private final TransactionStatisticService statisticService;
 
 	public TransactionStatisticController(TransactionStatisticService statisticService) {
 		this.statisticService = statisticService;
+	}
+
+	@GetMapping("/aggregate")
+	public ResponseEntity<TransactionStatisticResult> aggregate(@RequestParam Instant theDay) {
+		return ResponseEntity.ok(statisticService.aggregate(theDay));
 	}
 }
