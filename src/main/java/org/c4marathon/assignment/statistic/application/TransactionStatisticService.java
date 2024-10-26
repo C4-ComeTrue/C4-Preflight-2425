736a4edef0ed5dc17bf0c954603bc9cc8c7c61d6
ZZAMBAs <rksidksrksi@naver.com>
2024-10-26 11:59:05 +0900

    feat: 전에 없던 통계까지 벌크 삽입

--- src/main/java/org/c4marathon/assignment/statistic/application/TransactionStatisticService.java
+++ src/main/java/org/c4marathon/assignment/statistic/application/TransactionStatisticService.java
@@ -3,15 +3,19 @@ package org.c4marathon.assignment.statistic.application;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.TreeMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
 
-import org.c4marathon.assignment.global.util.CustomThreadPoolExecutor;
 import org.c4marathon.assignment.global.util.QueryTemplate;
 import org.c4marathon.assignment.statistic.domain.TransactionStatistic;
 import org.c4marathon.assignment.statistic.domain.TransactionStatisticRepository;
@@ -31,87 +35,67 @@ public class TransactionStatisticService {
 
 	private final TransactionStatisticRepository statisticRepository;
 	private final TransactionRepository transactionRepository;
-	private final CustomThreadPoolExecutor threadPoolExecutor;
 	private final TransactionStatisticRepository transactionStatisticRepository;
 
 	public TransactionStatisticService(TransactionStatisticRepository statisticRepository,
-		TransactionRepository transactionRepository, CustomThreadPoolExecutor threadPoolExecutor,
-		TransactionStatisticRepository transactionStatisticRepository) {
+		TransactionRepository transactionRepository, TransactionStatisticRepository transactionStatisticRepository) {
 		this.statisticRepository = statisticRepository;
 		this.transactionRepository = transactionRepository;
-		this.threadPoolExecutor = threadPoolExecutor;
 		this.transactionStatisticRepository = transactionStatisticRepository;
 	}
 
 	public TransactionStatisticResult aggregate(Instant theDay) {
-		// 0. 통계 테이블에서 집계하려는 날짜의 데이터가 있으면 그걸 반환.
 		Optional<TransactionStatistic> statisticOptional = statisticRepository.findCloseStatisticByStatisticDate(theDay);
 
 		if (statisticOptional.isPresent() && Objects.equals(statisticOptional.get().getStatisticDate(), theDay)) {
 			return TransactionStatisticResult.from(statisticOptional.get());
 		}
 
-		// 1. 통계 테이블에서 그 날짜와 제일 가까운 통계 있는지 확인.
-		// 1-1. 없으면 트랜잭션 테이블 제일 첫 거래 날짜를 가져온다. -> 다음날 자정으로 기록한다.
-		// 		근데 자정을 어떻게 설정할 수 있을까 -> toEpochSecond()로 표현한 뒤, % (3600 * 24)로 나온 값을 짜르자!
-		// 1-1-1 트랜잭션도 없으면 그냥 지금 시간으로 기록한다(가져올 데이터 없도록)
-		// 1-2. 있으면 그것을 가져오고 날짜와 누적 금액에 추가한다.
-
 		Instant endDay = getMidnightOfDay(theDay).plus(1L, ChronoUnit.DAYS);
-		AtomicLong theDayAmount = new AtomicLong(0L);
-		AtomicLong cumulativeAmount = new AtomicLong(0L);
+		long cumulativeAmount = 0L;
 		Instant startDay;
 
 		if (statisticOptional.isPresent()) {
 			TransactionStatistic transactionStatistic = statisticOptional.get();
 			startDay = transactionStatistic.getStatisticDate().plus(1L, ChronoUnit.DAYS);
-			cumulativeAmount.addAndGet(transactionStatistic.getCumulativeAmount());
+			cumulativeAmount = transactionStatistic.getCumulativeAmount();
 		} else {
 			Transaction firstTransaction = transactionRepository.findFirstOrderByTransactionDate().orElse(null);
 			startDay = firstTransaction == null ? Instant.now() : getMidnightOfDay(firstTransaction.getTransactionDate());
 		}
 
-		// 2. theDay 전까지 스레드 풀 각 스레드만큼 3시간 단위(비교하며 적절히 쪼개본다)로 분리하고, 배치로 Transaction 테이블 돌면서 다음을 수행
-		// 2-1. cumulativeAmount에 추가
-		// 2-2. 현재 Instant가 (theDay 그 자정)보다 같거나 크면 theDayAmount에 추가
-
 		List<Instant> splitTimes = getSplitTimes(startDay, endDay);
+
+		Map<Instant, AtomicLong> statisticMap = new TreeMap<>(Comparator.comparing(Instant::getEpochSecond));
+		splitTimes.forEach(splitTime -> statisticMap.putIfAbsent(getMidnightOfDay(splitTime), new AtomicLong()));
 
 		ExecutorService executorService = Executors.newFixedThreadPool(8);
 
 		splitTimes.forEach(splitTime -> executorService.execute(() ->
 			QueryTemplate.<Transaction>selectAndExecuteWithCursor(BATCH_SIZE,
 				transaction -> transactionRepository.findBetweenTimesWithCursor(transaction == null ? splitTime : transaction.getTransactionDate(), splitTime.plusSeconds(SPLIT_SECONDS_PER_THREADS), transaction == null ? null : transaction.getId(), BATCH_SIZE),
-				transactions -> transactions.forEach(transaction -> calculateStatistic(theDay, transaction, cumulativeAmount, theDayAmount)))
+				transactions -> transactions.forEach(transaction -> calculateStatistic(transaction, statisticMap)))
 			)
 		);
 
-		//threadPoolExecutor.waitToEnd();
 		executorService.shutdown();
 		try {
-			executorService.awaitTermination(1L, TimeUnit.HOURS);
+			executorService.awaitTermination(20L, TimeUnit.MINUTES);
 		} catch (Exception e) {
 			log.warn("스레드 풀이 정상적으로 종료되지 않았습니다.");
 		}
 
-		TransactionStatistic transactionStatistic = TransactionStatistic.builder()
-			.dailyTotalAmount(theDayAmount.get())
-			.cumulativeAmount(cumulativeAmount.get())
-			.statisticDate(getMidnightOfDay(theDay))
-			.build();
+		List<TransactionStatistic> statistics = getStatistics(statisticMap, cumulativeAmount);
 
-		transactionStatisticRepository.save(transactionStatistic);
+		transactionStatisticRepository.saveAll(statistics);
 
-		return TransactionStatisticResult.from(transactionStatistic);
+		return TransactionStatisticResult.from(statistics.get(statistics.size() - 1));
 	}
 
-	private void calculateStatistic(Instant theDay, Transaction transaction, AtomicLong cumulativeAmount,
-		AtomicLong theDayAmount) {
-		cumulativeAmount.addAndGet(transaction.getAmount());
+	private void calculateStatistic(Transaction transaction, Map<Instant, AtomicLong> statisticMap) {
+		Instant curTransactionDate = getMidnightOfDay(transaction.getTransactionDate());
 
-		if (isSameDay(transaction.getTransactionDate(), theDay)) {
-			theDayAmount.addAndGet(transaction.getAmount());
-		}
+		statisticMap.get(curTransactionDate).addAndGet(transaction.getAmount());
 	}
 
 	private Instant getMidnightOfDay(Instant time) {
@@ -129,7 +113,20 @@ public class TransactionStatisticService {
 		return splitTimes;
 	}
 
-	private boolean isSameDay(Instant instant1, Instant instant2) {
-		return getMidnightOfDay(instant1).equals(getMidnightOfDay(instant2));
+	private List<TransactionStatistic> getStatistics(Map<Instant, AtomicLong> statisticMap, long cumulativeAmount) {
+		List<TransactionStatistic> statistics = new ArrayList<>();
+		for (var entry : statisticMap.entrySet()) {
+			cumulativeAmount += entry.getValue().get();
+
+			TransactionStatistic transactionStatistic = TransactionStatistic.builder()
+				.statisticDate(entry.getKey())
+				.dailyTotalAmount(entry.getValue().get())
+				.cumulativeAmount(cumulativeAmount)
+				.build();
+
+			statistics.add(transactionStatistic);
+		}
+
+		return statistics;
 	}
 }
--- src/main/java/org/c4marathon/assignment/statistic/domain/TransactionStatisticRepository.java
+++ src/main/java/org/c4marathon/assignment/statistic/domain/TransactionStatisticRepository.java
@@ -1,17 +1,22 @@
 package org.c4marathon.assignment.statistic.domain;
 
 import java.time.Instant;
+import java.util.List;
 import java.util.Optional;
 
 import org.c4marathon.assignment.statistic.infrastructure.TransactionStatisticJpaRepository;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class TransactionStatisticRepository {
 	private final TransactionStatisticJpaRepository statisticJpaRepository;
+	private final JdbcTemplate jdbcTemplate;
 
-	public TransactionStatisticRepository(TransactionStatisticJpaRepository statisticJpaRepository) {
+	public TransactionStatisticRepository(TransactionStatisticJpaRepository statisticJpaRepository,
+		JdbcTemplate jdbcTemplate) {
 		this.statisticJpaRepository = statisticJpaRepository;
+		this.jdbcTemplate = jdbcTemplate;
 	}
 
 	public Optional<TransactionStatistic> findCloseStatisticByStatisticDate(Instant theDay) {
@@ -20,5 +25,18 @@ public class TransactionStatisticRepository {
 
 	public void save(TransactionStatistic transactionStatistic) {
 		statisticJpaRepository.save(transactionStatistic);
+	}
+
+	public void saveAll(List<TransactionStatistic> transactionStatistics) {
+		String sql = """
+			INSERT INTO transaction_statistic_zzamba (statistic_date, daily_total_amount, cumulative_amount)
+			VALUES (?, ?, ?)
+			""";
+
+		jdbcTemplate.batchUpdate(sql, transactionStatistics, transactionStatistics.size(), (ps, statistic) -> {
+			ps.setObject(1, statistic.getStatisticDate());
+			ps.setLong(2, statistic.getDailyTotalAmount());
+			ps.setLong(3, statistic.getCumulativeAmount());
+		});
 	}
 }
