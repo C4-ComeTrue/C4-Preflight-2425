6b524b1fdc078c903fb10f10bdb712a9740b9408
nyh365 <12nyk34@naver.com>
2024-10-31 22:56:06 +0900

    feat: 발송 대기 중인 이메일 조회 추가

--- src/main/java/org/c4marathon/assignment/repository/EmailBoxJpaRepository.java
+++ src/main/java/org/c4marathon/assignment/repository/EmailBoxJpaRepository.java
@@ -1,7 +1,10 @@
 package org.c4marathon.assignment.repository;
 
 import org.c4marathon.assignment.entity.EmailBox;
+import org.c4marathon.assignment.entity.EmailStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface EmailBoxJpaRepository extends JpaRepository<EmailBox, Long> {
+    List<EmailBox> findEmailBoxesByStatus(EmailStatus status);
 }
