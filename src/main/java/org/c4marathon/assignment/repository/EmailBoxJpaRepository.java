78188096d9ec2a45893990335661086c9bfefba4
nyh365 <12nyk34@naver.com>
2024-10-31 22:54:51 +0900

    refactor: 파일명 수정

--- src/main/java/org/c4marathon/assignment/repository/EmailBoxJpaRepository.java
+++ src/main/java/org/c4marathon/assignment/repository/EmailBoxJpaRepository.java
@@ -3,5 +3,5 @@ package org.c4marathon.assignment.repository;
 import org.c4marathon.assignment.entity.EmailBox;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EmailBoxRepository extends JpaRepository<EmailBox, Long> {
+public interface EmailBoxJpaRepository extends JpaRepository<EmailBox, Long> {
 }
