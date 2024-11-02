fea692545e4a10d2cc920b928e00becf27fecfb9
nyh365 <12nyk34@naver.com>
2024-11-03 01:35:20 +0900

    feat: 메일 발송 기능 추가

--- src/main/java/org/c4marathon/assignment/service/EmailService.java
+++ src/main/java/org/c4marathon/assignment/service/EmailService.java
@@ -1,9 +1,13 @@
 package org.c4marathon.assignment.service;
 
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.c4marathon.assignment.dto.request.PostEmailBoxReq;
+import org.c4marathon.assignment.entity.EmailBox;
 import org.c4marathon.assignment.repository.EmailBoxRepository;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,8 +15,25 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class EmailService {
     private final EmailBoxRepository emailBoxRepository;
+    private final JavaMailSender javaMailSender;
 
     public void postEmailBox(PostEmailBoxReq postEmailBoxReq) {
         emailBoxRepository.postEmailBox(postEmailBoxReq);
+    }
+
+    public Long makeEmail(EmailBox emailBox) {
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        try {
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, false);
+            mimeMessageHelper.setTo(emailBox.getEmail());
+            mimeMessageHelper.setSubject("[계좌 상품 설명서]");
+            mimeMessageHelper.setText(emailBox.getContent());
+
+            javaMailSender.send(mimeMessage);
+            return emailBox.getId();
+        } catch (Exception e) {
+            log.error("[메일 전송 실패]: [{}], {}", emailBox.getId(), e.getMessage());
+        }
+        return null;
     }
 }
