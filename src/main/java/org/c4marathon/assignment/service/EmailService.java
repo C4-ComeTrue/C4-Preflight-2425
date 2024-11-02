6154cf74a58715a16dc1693aa0750c541bc79cf2
nyh365 <12nyk34@naver.com>
2024-11-03 01:31:56 +0900

    feat: 로깅 추가

--- src/main/java/org/c4marathon/assignment/service/EmailService.java
+++ src/main/java/org/c4marathon/assignment/service/EmailService.java
@@ -1,12 +1,14 @@
 package org.c4marathon.assignment.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.c4marathon.assignment.dto.request.PostEmailBoxReq;
 import org.c4marathon.assignment.repository.EmailBoxRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class EmailService {
     private final EmailBoxRepository emailBoxRepository;
 
