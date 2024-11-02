cc97d6241e8f04982377489eb97e421d52649f67
nyh365 <12nyk34@naver.com>
2024-11-03 01:31:21 +0900

    refactor: 레포지토리 변경에 따른 수정

--- src/main/java/org/c4marathon/assignment/service/EmailService.java
+++ src/main/java/org/c4marathon/assignment/service/EmailService.java
@@ -2,7 +2,6 @@ package org.c4marathon.assignment.service;
 
 import lombok.RequiredArgsConstructor;
 import org.c4marathon.assignment.dto.request.PostEmailBoxReq;
-import org.c4marathon.assignment.entity.EmailBox;
 import org.c4marathon.assignment.repository.EmailBoxRepository;
 import org.springframework.stereotype.Service;
 
@@ -11,10 +10,7 @@ import org.springframework.stereotype.Service;
 public class EmailService {
     private final EmailBoxRepository emailBoxRepository;
 
-    public void postEmailLog(PostEmailBoxReq postEmailBoxReq) {
-        emailBoxRepository.save(EmailBox.builder()
-                                        .email(postEmailBoxReq.email())
-                                        .content(postEmailBoxReq.accountDescription())
-                                        .build());
+    public void postEmailBox(PostEmailBoxReq postEmailBoxReq) {
+        emailBoxRepository.postEmailBox(postEmailBoxReq);
     }
 }
