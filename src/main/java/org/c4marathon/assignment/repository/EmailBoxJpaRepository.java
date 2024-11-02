1291b329ee96a21246f11651ff5dc5dbb9205201
nyh365 <12nyk34@naver.com>
2024-11-03 01:39:47 +0900

    feat: 이메일 발송 상태 변경 추가

--- src/main/java/org/c4marathon/assignment/repository/EmailBoxJpaRepository.java
+++ src/main/java/org/c4marathon/assignment/repository/EmailBoxJpaRepository.java
@@ -3,8 +3,22 @@ package org.c4marathon.assignment.repository;
 import org.c4marathon.assignment.entity.EmailBox;
 import org.c4marathon.assignment.entity.EmailStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.List;
 
 public interface EmailBoxJpaRepository extends JpaRepository<EmailBox, Long> {
+
     List<EmailBox> findEmailBoxesByStatus(EmailStatus status);
+
+    @Transactional
+    @Modifying
+    @Query(value = """
+			UPDATE EmailBox e
+			SET e.status = :status
+			WHERE e.id IN :ids
+		""")
+    void updateStatus(EmailStatus status, List<Long> ids);
 }
