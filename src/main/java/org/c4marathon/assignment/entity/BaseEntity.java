3a3619d90dcd67a8687581bf8ca5f7a05f5b5504
nyh365 <12nyk34@naver.com>
2024-11-03 06:00:13 +0900

    style: 개행문자 제거

--- src/main/java/org/c4marathon/assignment/entity/BaseEntity.java
+++ src/main/java/org/c4marathon/assignment/entity/BaseEntity.java
@@ -6,7 +6,6 @@ import jakarta.persistence.MappedSuperclass;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
 import java.time.LocalDateTime;
 
 @EntityListeners(AuditingEntityListener.class)
--- src/main/java/org/c4marathon/assignment/service/EmailService.java
+++ src/main/java/org/c4marathon/assignment/service/EmailService.java
@@ -11,7 +11,6 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
