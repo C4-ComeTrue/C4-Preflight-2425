439b102995da974c07055e79b96354f3a787fdad
nyh365 <12nyk34@naver.com>
2024-11-06 04:25:47 +0900

    feat: 당일 총 송금액 조회 기능

---
+++ src/main/java/org/c4marathon/assignment/repository/TransactionJpaRepository.java
@@ -0,0 +1,17 @@
+package org.c4marathon.assignment.repository;
+
+import java.time.Instant;
+
+import org.c4marathon.assignment.entity.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface TransactionJpaRepository extends JpaRepository<Transaction, Long> {
+
+	@Query("""
+		SELECT COALESCE(SUM(t.amount), 0)
+		FROM Transaction t WHERE :startDate <= t.transactionDate AND t.transactionDate < :endDate
+		""")
+	Long sumAmountBetweenDate(@Param("startDate") Instant startInstantDate, @Param("endDate") Instant endInstantDate);
+}
---
+++ src/main/java/org/c4marathon/assignment/repository/TransactionRepository.java
@@ -0,0 +1,19 @@
+package org.c4marathon.assignment.repository;
+
+import java.time.Instant;
+import java.util.List;
+
+import org.c4marathon.assignment.entity.Transaction;
+import org.springframework.stereotype.Repository;
+
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
+public class TransactionRepository {
+	private final TransactionJpaRepository transactionJpaRepository;
+
+	public Long sumAmountBetweenDate(Instant startInstantDate, Instant endInstantDate) {
+		return transactionJpaRepository.sumAmountBetweenDate(startInstantDate, endInstantDate);
+	}
+}
