42c09b9c8d499910fdb53356819468baf667780d
nyh365 <12nyk34@naver.com>
2024-11-06 04:38:11 +0900

    style: 개행 문자 수정

--- src/main/java/org/c4marathon/assignment/controller/StatisticController.java
+++ src/main/java/org/c4marathon/assignment/controller/StatisticController.java
@@ -1,1 +1,32 @@
-package org.c4marathon.assignment.controller;import java.time.LocalDate;import java.util.List;import org.c4marathon.assignment.dto.response.StatisticRes;import org.c4marathon.assignment.service.StatisticService;import org.springframework.http.ResponseEntity;import org.springframework.web.bind.annotation.GetMapping;import org.springframework.web.bind.annotation.RequestMapping;import org.springframework.web.bind.annotation.RequestParam;import org.springframework.web.bind.annotation.RestController;import lombok.RequiredArgsConstructor;@RestController@RequestMapping("/statistic")@RequiredArgsConstructorpublic class StatisticController {	private final StatisticService statisticService;	/**	 시작 날짜와 종료 날짜를 받아 테이블 조회하는 API	 */	@GetMapping("/range")	public ResponseEntity<List<StatisticRes>> getStatisticByDate(		@RequestParam LocalDate startDate,		@RequestParam LocalDate endDate	) {		return ResponseEntity.ok(statisticService.getStatisticsByDateRange(startDate, endDate));	}}
+package org.c4marathon.assignment.controller;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.c4marathon.assignment.dto.response.StatisticRes;
+import org.c4marathon.assignment.service.StatisticService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/statistic")
+@RequiredArgsConstructor
+public class StatisticController {
+	private final StatisticService statisticService;
+
+	/**
+	 시작 날짜와 종료 날짜를 받아 테이블 조회하는 API
+	 */
+	@GetMapping("/range")
+	public ResponseEntity<List<StatisticRes>> getStatisticByDate(
+		@RequestParam LocalDate startDate,
+		@RequestParam LocalDate endDate
+	) {
+		return ResponseEntity.ok(statisticService.getStatisticsByDateRange(startDate, endDate));
+	}
+}
