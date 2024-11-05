6bd91e779b5255165c09f9622ea49c68c9e7d574
nyh365 <12nyk34@naver.com>
2024-11-06 04:08:55 +0900

    feat: 타입 수정 및 유효성 검사 추가

--- src/main/java/org/c4marathon/assignment/entity/BaseEntity.java
+++ src/main/java/org/c4marathon/assignment/entity/BaseEntity.java
@@ -3,18 +3,23 @@ package org.c4marathon.assignment.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-import java.time.LocalDateTime;
 
+import java.time.Instant;
+
+@Getter
 @EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
 public class BaseEntity {
-    @CreatedDate
-    @Column(updatable = false)
-    private LocalDateTime createdAt;
+	@CreatedDate
+	@Column(updatable = false, nullable = false)
+	private Instant createdAt;
 
-    @LastModifiedDate
-    private LocalDateTime updatedAt;
+	@LastModifiedDate
+	@Column(nullable = false)
+	private Instant updatedAt;
 }
