587647a16453a1cf445f3821ea02c0a6f0eafc7b
fingersdanny <kbry12@naver.com>
2024-11-09 14:49:47 +0900

    feat : 레코드를 PROCESSING으로 변경하는 로직 추가 - 이후 다시 조회 시 PENDING인 건들만 조회하여 처리할 수 있도록 변경

--- src/main/java/org/c4marathon/assignment/domain/email_record/service/EmailRecordService.java
+++ src/main/java/org/c4marathon/assignment/domain/email_record/service/EmailRecordService.java
@@ -1,12 +1,20 @@
 package org.c4marathon.assignment.domain.email_record.service;
 
+import static org.c4marathon.assignment.common.configuration.AsyncConfig.*;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
 import org.c4marathon.assignment.domain.email_record.dto.EmailRecordRequest;
 import org.c4marathon.assignment.domain.email_record.entity.EmailRecord;
 import org.c4marathon.assignment.domain.email_record.event.EmailRecordEvent;
+import org.c4marathon.assignment.domain.email_record.query.EmailRecordQueryResult;
 import org.c4marathon.assignment.domain.email_record.repository.EmailRecordReader;
 import org.c4marathon.assignment.domain.email_record.repository.EmailRecordStore;
 import org.c4marathon.assignment.domain.member.repository.MemberReader;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -31,7 +39,17 @@ public class EmailRecordService {
 	@Transactional(readOnly = true)
 	public void sendEmail() {
 		emailRecordReader.findAllPending()
-			.map(EmailRecordEvent::toEvent)
+			.map(result -> {
+				setStatusToProcessing(result);
+				return EmailRecordEvent.toEvent(result);
+			})
 			.forEach(eventPublisher::publishEvent);
+	}
+
+	@Async(value = ASYNC_THREAD_POOL_NAME)
+	@Transactional
+	public void setStatusToProcessing(EmailRecordQueryResult result) {
+		EmailRecord emailRecord = emailRecordReader.find(result.getId());
+		emailRecord.setStatusToProcessing();
 	}
 }
