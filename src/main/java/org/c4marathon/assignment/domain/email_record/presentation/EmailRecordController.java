ee80abf8d3f0aed39896476033fe981676489a2e
fingersdanny <kbry12@naver.com>
2024-11-09 12:56:20 +0900

    refactor : BaseURL 변동성을 고려하여 필드에서 주입 받아서 처리하는 방식으로 변경

--- src/main/java/org/c4marathon/assignment/domain/email_record/presentation/EmailRecordController.java
+++ src/main/java/org/c4marathon/assignment/domain/email_record/presentation/EmailRecordController.java
@@ -5,6 +5,7 @@ import java.net.URI;
 import org.c4marathon.assignment.domain.email_record.dto.EmailRecordRequest;
 import org.c4marathon.assignment.domain.email_record.entity.EmailRecord;
 import org.c4marathon.assignment.domain.email_record.service.EmailRecordService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,10 +21,13 @@ import lombok.RequiredArgsConstructor;
 public class EmailRecordController {
 	private final EmailRecordService emailRecordService;
 
+	@Value("${api.email-record.base-url}")
+	private String baseUrl;
+
 	@PostMapping("/v1/email-records")
 	public ResponseEntity<EmailRecord> saveEmailRecord(@Valid EmailRecordRequest.Save request) {
 		EmailRecord saved = emailRecordService.save(request);
-		URI uri = UriComponentsBuilder.fromUriString("http://localhost:8080/api/v1/email-records/" + saved.getId())
+		URI uri = UriComponentsBuilder.fromUriString(baseUrl + saved.getId())
 			.build()
 			.toUri();
 		return ResponseEntity.created(uri).build();
