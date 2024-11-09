9d27816b79e01ae621fee11a5eaa597e3f3ad5eb
fingersdanny <kbry12@naver.com>
2024-11-09 12:29:54 +0900

    fix : subject 길이에 대한 유효성 검증 추가

--- src/main/java/org/c4marathon/assignment/domain/email_record/dto/EmailRecordRequest.java
+++ src/main/java/org/c4marathon/assignment/domain/email_record/dto/EmailRecordRequest.java
@@ -6,6 +6,7 @@ import org.c4marathon.assignment.domain.member.entity.Member;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 
 public class EmailRecordRequest {
 	public record Save(
@@ -15,6 +16,7 @@ public class EmailRecordRequest {
 		@NotBlank
 		String content,
 		@NotBlank
+		@Size(max = 50)
 		String subject
 	) {
 		public EmailRecord toEntity(Member member) {
