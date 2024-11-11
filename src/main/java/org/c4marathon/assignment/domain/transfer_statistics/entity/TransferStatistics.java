f089469256fc8a933eee811ba19c750d3f04ae4f
fingersdanny <kbry12@naver.com>
2024-11-12 00:46:37 +0900

    feat : 송금 통계 테이블 추가

---
+++ src/main/java/org/c4marathon/assignment/domain/transfer_statistics/entity/TransferStatistics.java
@@ -0,0 +1,42 @@
+package org.c4marathon.assignment.domain.transfer_statistics.entity;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import org.c4marathon.assignment.common.entity.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "transfer_statistics_hodadako")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class TransferStatistics extends BaseEntity {
+	@Column(name = "daily_total_amount", columnDefinition = "decimal(15, 2)")
+	private BigDecimal dailyTotalAmount;
+
+	@Column(name = "cumulative_total_amount", columnDefinition = "decimal(15, 2)")
+	private BigDecimal cumulativeTotalAmount;
+
+	@Column(name = "unit_date", columnDefinition = "timestamp")
+	private LocalDateTime unitDate;
+
+	@Override
+	public boolean equals(Object o) {
+		if (o instanceof TransferStatistics transferStatistics) {
+			return this.getId() == transferStatistics.getId();
+		}
+		return false;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(this);
+	}
+}
