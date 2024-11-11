62f666b6cc2bee05b7b7c635e02ec5f3b7fded36
fingersdanny <kbry12@naver.com>
2024-11-12 04:06:18 +0900

    fix : Transaction 테이블과 일치하도록 수정

--- src/main/java/org/c4marathon/assignment/domain/transaction/entity/Transaction.java
+++ src/main/java/org/c4marathon/assignment/domain/transaction/entity/Transaction.java
@@ -1,30 +1,50 @@
 package org.c4marathon.assignment.domain.transaction.entity;
 
-import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.Objects;
 
 import org.c4marathon.assignment.common.entity.BaseEntity;
+import org.hibernate.annotations.ColumnDefault;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "transaction_hodadako")
+@Table(name = "transaction", indexes = {})
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Transaction extends BaseEntity {
-	@Enumerated(EnumType.STRING)
-	@Column(name = "transaction_type", columnDefinition = "varchar(20)")
-	private TransactionType transactionType;
+	@Column(name = "sender_account", columnDefinition = "varchar(20)", nullable = false)
+	private String senderAccount;
 
-	@Column(name = "amount", columnDefinition = "decimal(15, 2)")
-	private BigDecimal amount;
+	@Column(name = "receiver_account", columnDefinition = "varchar(20)", nullable = false)
+	private String receiverAccount;
+
+	@Column(name = "sender_swift_code", columnDefinition = "varchar(11)", nullable = false)
+	private String senderSwiftCode;
+
+	@Column(name = "receiver_swift_code", columnDefinition = "varchar(11)", nullable = false)
+	private String receiverSwiftCode;
+
+	@Column(name = "sender_name", columnDefinition = "varchar(30)", nullable = false)
+	private String senderName;
+
+	@Column(name = "receiver_name", columnDefinition = "varchar(30)", nullable = false)
+	private String receiverName;
+
+	@Column(name = "amount", nullable = false)
+	private Long amount;
+
+	@Column(name = "memo", columnDefinition = "varchar(200)")
+	private String memo;
+
+	@ColumnDefault("CURRENT_TIMESTAMP")
+	@Column(name = "transaction_date", nullable = false)
+	private LocalDateTime transactionDate;
 
 	@Override
 	public boolean equals(Object o) {
