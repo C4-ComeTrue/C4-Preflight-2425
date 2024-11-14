686c9fde9874c96a9c77100a6741e5fe04fa4d53
nyh365 <12nyk34@naver.com>
2024-11-14 22:38:50 +0900

    feat: 사용자의 모든 계좌 정보와 모든 거래 내역을 제공하는 기능 추가
    - 각 계좌마다 연관된 거래 내역을 가져오는 과정에서 병목이 있었습니다.
    - A라는 계좌와 연관된 거래 내역을 모두 가져오기까지 기다리는 것이 아니라 동시에 각 계좌와 연관된 거래 내역을 모두 가져오게끔 했습니다.
    - CompletableFutre를 이용하여 각 계좌마다 연관된 거래 내역을 가져오는 과정을 비동기적으로 구현하였습니다.

---
+++ src/main/java/org/c4marathon/assignment/controller/MarathonController.java
@@ -0,0 +1,22 @@
+package org.c4marathon.assignment.controller;
+
+import org.c4marathon.assignment.dto.response.FinancialInfoRes;
+import org.c4marathon.assignment.service.MarathonService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/marathon")
+@RequiredArgsConstructor
+public class MarathonController {
+	private final MarathonService marathonService;
+
+	@GetMapping("/all-info")
+	public FinancialInfoRes getAllInfo(@RequestParam String email) {
+		return marathonService.getAllInfo(email);
+	}
+}
---
+++ src/main/java/org/c4marathon/assignment/service/MarathonService.java
@@ -0,0 +1,52 @@
+package org.c4marathon.assignment.service;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+
+import org.c4marathon.assignment.dto.response.AccountRes;
+import org.c4marathon.assignment.dto.response.FinancialInfoRes;
+import org.c4marathon.assignment.dto.response.TransactionRes;
+import org.c4marathon.assignment.entity.User;
+import org.c4marathon.assignment.exception.BadRequestException;
+import org.c4marathon.assignment.repository.AccountRepository;
+import org.c4marathon.assignment.repository.TransactionRepository;
+import org.c4marathon.assignment.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class MarathonService {
+	private final UserRepository userRepository;
+	private final AccountRepository accountRepository;
+	private final Executor asyncTaskExecutor;
+	private final TransactionRepository transactionRepository;
+
+	public FinancialInfoRes getAllInfo(String email) {
+		User user = userRepository.findUserByEmail(email)
+			.orElseThrow(() -> new BadRequestException("존재하지 않는 회원입니다."));
+
+		List<AccountRes> accountResList = accountRepository.findAllAccountByUserId(user.getId())
+			.stream().map(AccountRes::new).toList();
+
+		List<CompletableFuture<List<TransactionRes>>> futures = accountResList.stream()
+			.map(accountRes -> CompletableFuture.supplyAsync(() ->
+					transactionRepository.findAllTransaction(accountRes.accountNumber())
+						.stream()
+						.map(TransactionRes::new)
+						.toList(),
+				asyncTaskExecutor))
+			.toList();
+
+		List<TransactionRes> transactions = futures.stream()
+			.map(CompletableFuture::join)
+			.filter(Objects::nonNull)
+			.flatMap(List::stream)
+			.toList();
+
+		return new FinancialInfoRes(accountResList, transactions);
+	}
+}
