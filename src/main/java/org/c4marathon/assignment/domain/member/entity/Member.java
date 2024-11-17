77ae6779b6b7c8ef2e5c61d33a371a72b9226e43
fingersdanny <kbry12@naver.com>
2024-11-17 12:37:59 +0900

    feat : API 개발에 필요한 entity 추가

--- src/main/java/org/c4marathon/assignment/domain/member/entity/Member.java
+++ src/main/java/org/c4marathon/assignment/domain/member/entity/Member.java
@@ -1,22 +1,50 @@
 package org.c4marathon.assignment.domain.member.entity;
 
-import org.c4marathon.assignment.common.entity.BaseEntity;
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "member_hodadako")
+@Table(name = "user")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Member extends BaseEntity {
+public class Member {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "user_id", columnDefinition = "int")
+	private Long id;
+
 	@Column(name = "email", columnDefinition = "varchar(30)", unique = true)
 	private String email;
 
-	@Column(name = "name", columnDefinition = "varchar(20)")
+	@Column(name = "username", columnDefinition = "varchar(30)")
 	private String name;
+
+	@Column(name = "nickname", columnDefinition = "varchar(10)")
+	private String nickname;
+
+	@Column(name = "group_id", columnDefinition = "int")
+	private Integer groupId;
+
+	@Column(name = "user_status", columnDefinition = "char")
+	private String userStatus;
+
+	@CreatedDate
+	@Column(name = "create_date", columnDefinition = "timestamp")
+	private LocalDateTime createDate;
+
+	@LastModifiedDate
+	@Column(name = "update_date", columnDefinition = "timestamp")
+	private LocalDateTime updateDate;
 }
